30a8de03e087a9041c223ef0e326e6fca7000d3e
Kabir <sagithedeveloper@gmail.com>
2025-06-02 18:58:20 +0530

    added the code

---
+++ S03_Variables/variables.js
@@ -0,0 +1,9 @@
+// var dummy = 99999999999;
+var city;
+city = "hyderabad";
+var id = 101;
+
+//below lines will print values
+// of city and id variable in terminal
+console.log(city, id);
+console.log("cdjkvkjdfvbdjkfvbdjkfvb");
